0ea27ac79b0e1a4f14edd125260e5c5720694da5
Eunseo Lee <el851@georgetown.edu>
2022-02-12 17:13:19 -0500

    !

---
+++ app/src/main/java/com/example/strayanimalrecords/EditorActvity.kt
@@ -0,0 +1,11 @@
+package com.example.strayanimalrecords
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditorActvity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_editor)
+    }
+}
--- app/src/main/java/com/example/strayanimalrecords/MainActivity.kt
+++ app/src/main/java/com/example/strayanimalrecords/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.strayanimalrecords
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import java.util.ArrayList
+
+lateinit var arrayAdapter: ArrayAdapter<*>
+var animalName = ArrayList<String>()
+var location = ArrayList<String>()
+var volunteerName = ArrayList<String>()
+var sex = ArrayList<String>()
+var neutered = ArrayList<String>()
+var animalType = ArrayList<String>()
+var healthStatus = ArrayList<Int>()
+lateinit var sharedPreferences: SharedPreferences
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-}
+    }//onCreate
+}//MainActivity
